a24c7a7045e693c7f7fab74df975f2632fe4dc58
Mike Clarke <mikec655@gmail.com>
2018-12-16 18:11:50 +0100

    Kleine aanpassing in Server class na maken class diagram

--- src/Server.java
+++ src/Server.java
@@ -5,12 +5,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit; 
   
-public class Server 
+public abstract class Server 
 { 
     private static final int PORT = 11000;
     private static final int AMOUNT_OF_CORES = 8;
     private static final int MAX_CLIENTS = 800;
 	private static int clientCounter = 0;
+	private static ScheduledExecutorService executor;
+	private static ServerSocket server;
   
     public static void main(String args[]) 
     { 
@@ -18,9 +20,9 @@ public class Server
     	Database.connect();
     	
     	// Aanmaken van een Threadpool met Scheduler
-    	ScheduledExecutorService exec = Executors.newScheduledThreadPool(MAX_CLIENTS + 2);
+    	executor = Executors.newScheduledThreadPool(MAX_CLIENTS + 2);
 		// JVM vragen voor Garbage Collection
-    	exec.scheduleAtFixedRate(new Runnable() {
+    	executor.scheduleAtFixedRate(new Runnable() {
 				@Override
 				public void run() {
 					System.gc();
@@ -39,18 +41,18 @@ public class Server
         Socket connection; // Client socket
 		try {
 			// Server socket die luistert naar nieuwe clients
-			ServerSocket server = new ServerSocket(PORT);
+			server = new ServerSocket(PORT);
 			System.out.println("Server Started, Waiting for clients.");
 			
 			while (true) {
 				// Accepteren van nieuwe clients
 				connection = server.accept();		
-				System.out.println("New client accepted.");
 				// Client in Thread stoppen
 				Thread client = new Thread(new Client(connection));
 				clientCounter++;
 				//Thread toevoegen aan Threadpool
-				exec.scheduleAtFixedRate(client, 1000 / (MAX_CLIENTS / AMOUNT_OF_CORES) * clientCounter, 1000, TimeUnit.MILLISECONDS);
+				executor.scheduleAtFixedRate(client, 1000 / (MAX_CLIENTS / AMOUNT_OF_CORES) * clientCounter, 1000, TimeUnit.MILLISECONDS);
+				System.out.println("New client accepted. client count: " + clientCounter);
 			}
 		}
 
