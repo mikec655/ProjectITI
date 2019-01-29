10169f1f820ad421f38670383e0c62fde4eecf56
gerben bunt <g.bunt@st.hanze.nl>
2019-01-29 15:23:35 +0100

    fix "str" sneller????

--- src/Client.java
+++ src/Client.java
@@ -49,36 +49,47 @@ public class Client implements Runnable{
     	long startTime = System.nanoTime();
 		int charIndex = 55;
 		int[] charIndexSteps = {14, 15, 15, 15, 14, 13, 15, 16, 15, 15, 17, 17, 17, 45};
-		char c;
+		
     	
     	int j = 0;
 		for (int i = 0; i < 10; i++) {
+			char c;
 			byte[] bytes = new byte[43];
 			String str = new String();
 			int num = 0;
 			float f = 0;
-	
+			int strIndex =0;
+			
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				//str = str.concat(c);
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
+			
+			//System.out.println(str);
 			charIndex += charIndexSteps[j++];
 			num = Integer.parseInt(str);
 			stations[i].setStn(num);
 
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				//str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			stations[i].setDate(str);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			DateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 			try {
@@ -92,11 +103,13 @@ public class Client implements Runnable{
 			bytes[2] = (byte) (num >> 8);
 			bytes[3] = (byte) (num);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			f = Float.parseFloat(str);
 			if (stations[i].sizeOfTemp() > 0) {
@@ -110,110 +123,130 @@ public class Client implements Runnable{
 			stations[i].addTemp(f);
 			addFloatToByteArray(bytes, 4, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].extrapolateDewp(); 
 			else f = Float.parseFloat(str);
 			stations[i].addDewp(f);
 			addFloatToByteArray(bytes, 8, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].extrapolateStp(); 
 			else f = Float.parseFloat(str);
 			stations[i].addStp(f);
 			addFloatToByteArray(bytes, 12, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].extrapolateSlp(); 
 			else f = Float.parseFloat(str);
 			stations[i].addSlp(f);
 			addFloatToByteArray(bytes, 16, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].getVisib(); 
 			else f = Float.parseFloat(str);
 			stations[i].setVisib(f);
 			addFloatToByteArray(bytes, 20, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].extrapolateWdsp();
 			else f = Float.parseFloat(str);
 			stations[i].addWdsp(f);
 			addFloatToByteArray(bytes, 24, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].getPrcp();
 			else f = Float.parseFloat(str);
 			stations[i].setPrcp(f);
 			addFloatToByteArray(bytes, 28, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].getSndp();
 			else f = Float.parseFloat(str);
 			stations[i].setSndp(f);
 			addFloatToByteArray(bytes, 32, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) num = stations[i].getFrshtt();
 			else num = Integer.parseInt(str, 2);
 			stations[i].setFrshtt(num);
 			bytes[36] = (byte) (num);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) f = stations[i].getCldc();
 			else f = Float.parseFloat(str);
 			stations[i].setCldc(f);
 			addFloatToByteArray(bytes, 37, f);
 			
+			strIndex = 0;
 			str = "";
 			while ((c = xml.charAt(charIndex)) != '<') {
-				str += c;
+				strIndex ++;
 				charIndex++;
 			}
+			str = xml.substring(charIndex-strIndex, charIndex);
 			
 			charIndex += charIndexSteps[j++];
 			if (str.isEmpty()) num = stations[i].getWnddir();
@@ -235,6 +268,7 @@ public class Client implements Runnable{
 		
 	}
 	
+
 	private static void addFloatToByteArray(byte[] array, int index, float var) {
 		byte[] newBytes = ByteBuffer.allocate(4).putFloat(var).array();
 		array[index + 0] = newBytes[0];
